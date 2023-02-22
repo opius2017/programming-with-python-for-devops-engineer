9ddb80894d5a76d3d57c5f7ccc37988abd91bedb
Owolabi <opius2007@gmail.com>
2023-02-22 05:07:22 +0100

    Global and Local variable declaration

---
+++ lesson2-python-basics/lab/reusable-variables/vars.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def hello(name):
+    print('Hello ' + name)
+
+
+name = sys.argv[1]
+hello(name)
+
+# # local variable
+# def hello():
+#     name = "Pius"
+#     print("Hello " + name)
+
+#     hello()
+
+
+# # global variable
+# name = "Pius"
+
+# def goodbye():
+#     print('Goodbye ' + name)
+
+#     goodbye()
