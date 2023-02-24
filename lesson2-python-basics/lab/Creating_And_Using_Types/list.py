9720f23892e56086141a5a40c901340b894b9715
Owolabi <opius2007@gmail.com>
2023-02-24 06:48:54 +0100

    dictionary Update

--- lesson2-python-basics/lab/Creating_And_Using_Types/list.py
+++ lesson2-python-basics/lab/Creating_And_Using_Types/list.py
@@ -1,3 +1,8 @@
 cars = ["Totota", "Honda", "Ford"]
 
+# index will print out one or part of its values
 print(cars[2] + " is my best car")
+
+# Dictionaries are key value pairs
+my_car = {"Make": "Ford", "Year": "2020"}
+print(my_car)
